9e128663d79e45d2cf42a1cef85a353a4ab91751
Mason <masonbush.dev@gmail.com>
2024-07-06 19:11:53 +0100

    added hmac signature check

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 import uuid
+import hmac
+import os
+import hashlib
 
 import discord
 import twitch
@@ -7,13 +10,39 @@ import twitch
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
 
+
+TWITCH_MESSAGE_ID = "Twitch-Eventsub-Message-Id"
+TWITCH_MESSAGE_TYPE = "Twitch-Eventsub-Message-Type"
+TWITCH_MESSAGE_TIMESTAMP = "Twitch-Eventsub-Message-Timestamp"
+TWITCH_MESSAGE_SIGNATURE = "Twitch-Eventsub-Message-Signature"
+
+
+def requiredHeaders(headers, requredHeaders):
+	for i in requredHeaders:
+		if not headers.get(i):
+			return False
+		
+	return True
+
+
+def getHmac(secret, message):
+	return hmac.new(str.encode(secret), str.encode(message), hashlib.sha256).hexdigest()
+
+
 @app.post("/")
 def post_webhook():
 
-	if "Twitch-Eventsub-Message-Type" in request.headers:
-		eventType = request.headers['Twitch-Eventsub-Message-Type']
+	if requiredHeaders(request.headers, [TWITCH_MESSAGE_ID, TWITCH_MESSAGE_TYPE, TWITCH_MESSAGE_TIMESTAMP, TWITCH_MESSAGE_SIGNATURE]):
+		eventType = request.headers[TWITCH_MESSAGE_TYPE]
 
+		message = request.headers[TWITCH_MESSAGE_ID] + request.headers[TWITCH_MESSAGE_TIMESTAMP] + request.data.decode('UTF-8')
+		hmac = getHmac(os.getenv("hmacSecret"), message)
 
+		if f"sha256={hmac}" != request.headers[TWITCH_MESSAGE_SIGNATURE]:
+			print("Incorrect signature")
+			return "", 403
+			
+			
 		match eventType:
 			case "webhook_callback_verification":
 				if "challenge" in request.json:
